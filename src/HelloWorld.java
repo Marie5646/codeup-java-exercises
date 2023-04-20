e82c231b3a0210a8660c8865e925fad2233c2b12
Asia Autrey <asia.autrey0@gmail.com>
2023-04-20 09:48:46 -0500

    exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,29 @@
 public class HelloWorld {
+
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Hi");
+
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+
+        int myFavoriteNumber= 8;
+        String myString = "asia";
+        long myNumber;
+//       myNumber= 3.14f;
+//       System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+
     }
 }
